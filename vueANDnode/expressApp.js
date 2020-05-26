8bbb8031f523bc1c40c5af57720d13b37d10d460
邓超 <dc870638530@163.com>
2020-05-26 18:07:08 +0800

    -

--- vueANDnode/expressApp.js
+++ vueANDnode/expressApp.js
@@ -3,3 +3,17 @@ var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 
 server.listen(8808)
+
+
+app.get('./',(req,res) => {
+    res.sendFile(__dirname + '/index.html',function(){
+        console.log('成功')
+    })
+})
+
+io.on('connection',function(socket){
+    socket.emit('news',{hello:'world'})
+    socket.on('myOtherEvent',function(data){
+        console.log(data)
+    })
+})
